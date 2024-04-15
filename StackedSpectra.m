077edb04cc1d44130316141f157b9a50ea58d54b
garaimate <garai.mate@gmail.com>
2024-04-15 16:03:22 -0500

    updated stacked spectra to be more general with finding the files

--- StackedSpectra.m
+++ StackedSpectra.m
@@ -3,14 +3,14 @@
 % TO DO: check that you're calling in only the files you want to plot. Use
 %an asterisk to inidate parts of the file name you don't want to specify.
 
-selFiles = dir('C:\Users\Mineral Physics\Desktop\SMP11 FTIR\new smp1 fft\fft*a.dat') ;
-
+selFiles = uigetdir('Select folder') ;
+selFiles = dir(strcat(selFiles,"/*.txt"));
 nFiles = numel(selFiles) ;
 
 yValue = [] ;
 
 for i = 1:nFiles
-    fileName = selFiles(i).name ;
+    fileName = strcat(selFiles(i).folder,"/",selFiles(i).name) ;
     
     tmpData = readmatrix(fileName) ;
     
@@ -24,15 +24,15 @@ for i = 1:nFiles
 end   
 
 %% make plots
-
+fig = figure();
 % TO DO: Indicate in the two lines below the range of X values to plot
 % for Raman centered at 547, a good range is 400 to 650
 % for Raman centered at 557 or 560, a good range is 950 to 1100
 % for Raman centered at 650, a good range is 3300 to 3600
 % for full FTIR spectra, a good range is 500 to 4500
 %for water region of FTIR only, try 2600 to 4000
-minXvalue = 950 ;
-maxXvalue = 1100 ;
+minXvalue = 500 ;
+maxXvalue = 4000 ;
 
 idx = find(xValue>=minXvalue & xValue<=maxXvalue) ;
 newXvalue = xValue(idx,:) ;
